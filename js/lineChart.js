d9383857ad019fc2a1b02700f00423fc41a83e77
Wentao Mao <psxwm4@exmail.nottingham.ac.uk>
2023-04-09 11:16:32 +0100

    Added interactive legend; Line would be highlighted if hover on the selected legend.

--- js/lineChart.js
+++ js/lineChart.js
@@ -4,8 +4,9 @@ class LineChart {
   // the line chart has one line for each selected location
   // when the user collected the line of a location, the AreaChart would render the area chart of the location
   // methods start with "_" are private methods, they should not be called outside the class
+  // ACKNOWLEDGEMENT
   // reference for general d3 https://michaeloppermann.com/work/d3
-  // reference for line chart https://michaeloppermann.com/d3-example/d3-line-chart
+  // reference for line chart https://michaeloppermann.com/d3-example/d3-interactive-line-chart
   // reference for ploting long data https://d3-graph-gallery.com/graph/line_several_group.html
   // code were constructed with the help of the reference and gitHub copilot as guidance
 
@@ -15,22 +16,50 @@ class LineChart {
     data,
     meanData,
     legendElement,
-    colours
+    colours,
+    changeStackedAreaChart
   ) {
     // initialize the class, save the parameters as properties
-    this.parentElement = parentElement;
-    this.tooltipElement = tooltipElement;
+    this.parentElement = parentElement; // id
+    this.tooltipElement = tooltipElement; // id
     this.data = data;
     this.filteredData = data; // data filtered by time range
     this.meanData = meanData; // mean data of the entire dataset
     this.filteredMeanData = meanData; // mean data filtered by time range
     this.locations = [];
-    this.locationsData = [];
-    this.legendElement = legendElement;
-    this.colours = colours;
-    this.CONTAINER_WIDTH = 1500;
+    this.locationsData = {};
+    this.legendDomElement = document.querySelector(legendElement); // dom element
+    this.legendElement = legendElement; // id
+    this.colours = colours.map((colour) => colour);
+    this.usedColours = [];
+    this.availableColours = colours.map((colour) => colour);
+    this.locationColour = {
+      1: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      2: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      3: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      4: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      5: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      6: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      7: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      8: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      9: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      10: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      11: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      12: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      13: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      14: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      15: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      16: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      17: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      18: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+      19: LINE_CHART_DEFAULT_LEGEND_COLOUR,
+    };
+    this.CONTAINER_WIDTH = 1000;
     this.CONTAINER_HEIGHT = 400;
-    this.MARGIN = { top: 20, right: 20, bottom: 20, left: 20 };
+    this.MARGIN = { top: 20, right: 20, bottom: 30, left: 40 };
+    this.timeStart = d3.min(this.data, (d) => d.time);
+    this.timeEnd = d3.max(this.data, (d) => d.time);
+    this.changeStackedAreaChart = changeStackedAreaChart;
   }
   initVis() {
     // initialize the axes, scales, svg, chart group, axes groups
@@ -72,21 +101,46 @@ class LineChart {
       );
 
     // drawing area for x axis and y axis
-    thisObj.xAxisG = thisObj.svg
+    thisObj.xAxisG = thisObj.chart
       .append("g")
       .attr("class", "axis x-axis")
       .attr("transform", `translate(0, ${thisObj.height})`);
-    thisObj.yAxisG = thisObj.svg.append("g").attr("class", "axis y-axis");
+    thisObj.yAxisG = thisObj.chart.append("g").attr("class", "axis y-axis");
 
-    //
+    // init legend
+    thisObj._initLegend();
+  }
+  _initLegend() {
+    // add event listeners to the linechart legend buttons
+    let thisObj = this;
+    for (let i = 0; i < thisObj.legendDomElement.children.length; i++) {
+      let legendItem = thisObj.legendDomElement.children[i];
+      // event listener for the showing and hiding of the line
+      legendItem.addEventListener("click", function () {
+        thisObj._toggleLocation(String(i + 1), true);
+      });
+      // event listener for highlighting the line when the mouse is over the legend
+      legendItem.addEventListener("mouseover", function () {
+        if (thisObj.locations.includes(String(i + 1))) {
+          thisObj.highLightLine(String(i + 1), true);
+        }
+      });
+      // event listenr for removing the highlight when the mouse is out of the legend
+      legendItem.addEventListener("mouseout", function () {
+        if (thisObj.locations.includes(String(i + 1))) {
+          thisObj.updateVis();
+          thisObj.renderVis();
+        }
+      });
+    }
   }
   updateVis() {
     // update the getter, line generator, scale, legend
     let thisObj = this;
-    console.log(thisObj._updateGetter());
+    thisObj._updateGetter();
     thisObj._updateLineGenerator();
     thisObj._updateScales();
-    // thisObj._updateLegend();
+    thisObj._updateLegend();
     return true;
   }
   _updateGetter(getX = (d) => d.time, getY = (d) => d.damage_value) {
@@ -105,8 +159,13 @@ class LineChart {
   _updateScales() {
     // update the scales: x scale, y scale, colour scale
     let thisObj = this;
+    // thisObj.xScale.domain(d3.extent(thisObj.filteredMeanData, thisObj.getX));
+    // thisObj.yScale.domain(d3.extent(thisObj.filteredMeanData, thisObj.getY));
     thisObj.xScale.domain(d3.extent(thisObj.filteredData, thisObj.getX));
-    thisObj.yScale.domain(d3.extent(thisObj.filteredData, thisObj.getY));
+    thisObj.yScale.domain([
+      0,
+      d3.max(thisObj.filteredData, (d) => d.damage_value),
+    ]);
   }
   _updateLineGenerator() {
     // update the line generator
@@ -118,6 +177,20 @@ class LineChart {
   }
   _updateLegend() {
     // update the legend
+    let thisObj = this;
+    // set colours for the selected locations
+    // thisObj.locations.forEach((location) => {
+    //   let q = `${thisObj.legendElement} #locationButton${location} .lineChartColourLegend`;
+    //   document.querySelector(q).style.backgroundColor =
+    //     thisObj.locationColour[Number(location)];
+    // });
+    // set colour for the locations
+    for (let i = 1; i <= NUMBER_OF_LOCATION; i++) {
+      let q = `${thisObj.legendElement} #locationButton${i} .lineChartColourLegend`;
+      console.log(q);
+      document.querySelector(q).style.backgroundColor =
+        thisObj.locationColour[i];
+    }
   }
   renderVis() {
     // render the axes, render the lines
@@ -125,12 +198,8 @@ class LineChart {
     // if there are locations, render the location lines and the mean line
     let thisObj = this;
     thisObj._renderAxes();
-    if (thisObj.locations.length === 0) {
-      thisObj._renderMeanLine();
-    } else {
-      thisObj._renderLocationLines();
-      thisObj._renderMeanLine();
-    }
+    thisObj._renderLocationLines();
+    thisObj._renderMeanLine();
     thisObj._renderAxes();
   }
   _renderAxes() {
@@ -147,15 +216,101 @@ class LineChart {
       .data([thisObj.filteredMeanData])
       .join("path")
       .attr("class", "mean-line")
-      .attr("d", thisObj.lineGenerator);
+      .attr("style", "stroke: black;")
+      .attr("d", thisObj.lineGenerator)
+      .style("stroke-width", 1);
   }
   _renderLocationLines() {
-    // render the location lines, use the colours in this.colours; if there are not locations, render nothing
+    // render the location lines, use this.locationColous, if the colour is green, only render the legend,
+    // else, render the line and the legend
+    let thisObj = this;
+    // remove the location lines that not in this.location
+    for (let i = 1; i <= NUMBER_OF_LOCATION; i++) {
+      if (!thisObj.locations.includes(toString(i))) {
+        console.log(`#line-${i} removed`);
+        d3.select(`#line-${i}`).remove();
+      }
+    }
     if (thisObj.locations.length === 0) {
       return false;
     } else {
       // render each lines
+      for (let location of thisObj.locations) {
+        thisObj.chart
+          .selectAll(`.location-line-${location}`)
+          .data([thisObj.locationsData[location]])
+          .join("path")
+          .attr("class", `location-line`)
+          .attr("id", `line-${location}`)
+          .attr("d", thisObj.lineGenerator)
+          .style("stroke", thisObj.locationColour[Number(location)])
+          .style("stroke-width", 1) // convert to int
+          .on("click", function (event) {
+            console.log(event);
+            console.log(this.id);
+            let location = this.id.split("-")[1];
+            thisObj.changeStackedAreaChart(
+              location,
+              thisObj.timeStart,
+              thisObj.timeEnd
+            );
+          });
+        // console.log(thisObj.locationColour[Number(location)]);
+      }
       return true;
+    }
+  }
+  _renderToolTip(time) {
+    // update the innerHTML of the this.tooltipElement
+  }
+  highLightLine(location, useHighlightColour = true) {
+    // set all the other line gray
+    // if the location is select, use it's assigned colour
+    // else, use the HIGHLIGHT_COLOUR and render a path for this location
+    // set the width of the line to be 2
+    // if useHighlightColour is true, call highLightLine2(location)
+    if (useHighlightColour) {
+      return this._highLightLine2(location);
+    }
+    let thisObj = this;
+    // set the meanLine to be gray
+    d3.select(".mean-line").style("stroke", "gray");
+    // set the selected location lines to be gray
+    thisObj.locations.forEach((location) => {
+      d3.select(`#line-${location}`).style("stroke", "gray");
+    });
+    // set the chosen location line to be the assigned colour if it is in thisObj.locations
+    // else, set it to be the HIGHLIGHT_COLOUR
+    if (thisObj.locations.includes(location)) {
+      d3.select(`#line-${location}`)
+        .style("stroke", thisObj.locationColour[Number(location)])
+        .style("stroke-width", 2);
+    } else {
+      d3.select(`#line-${location}`).style("stroke", HIGHLIGHT_COLOUR);
+    }
+  }
+  _highLightLine2(location) {
+    // set all the other line gray and at lower level
+    // if the location is select, use HIGHLIGHT_COLOUR
+
+    let thisObj = this;
+    // set the meanLine to be gray
+    d3.select(".mean-line").style("stroke", "gray").style("opacity", 0.5);
+    // set the selected location lines to be gray
+    thisObj.locations.forEach((location) => {
+      d3.select(`#line-${location}`)
+        .style("stroke", "gray")
+        .style("opacity", 0.5);
+    });
+    // set the chosen location line to be the assigned colour if it is in thisObj.locations
+    // else, set it to be the HIGHLIGHT_COLOUR
+    if (thisObj.locations.includes(location)) {
+      d3.select(`#line-${location}`)
+        .style("stroke", HIGHLIGHT_COLOUR)
+        .style("stroke-width", 2);
+      return true;
+    } else {
+      return false;
     }
   }
   _renderAxes() {
@@ -163,7 +318,7 @@ class LineChart {
     thisObj.xAxisG.call(thisObj.xAxis);
     thisObj.yAxisG.call(thisObj.yAxis);
   }
-  changeTime(start, end) {
+  changeTime(start, end, update = false) {
     // update the time range, change the filteredData and filteredMeanData
     let thisObj = this;
     thisObj.filteredData = thisObj.data.filter(
@@ -172,13 +327,139 @@ class LineChart {
     thisObj.filteredMeanData = thisObj.meanData.filter(
       (d) => d.time >= start && d.time <= end
     );
+    // update the location data
+    thisObj.locationsData = {};
+    thisObj.filterLocations(thisObj.locations, false);
+    if (update) {
+      thisObj.updateVis();
+      thisObj.renderVis();
+    }
+    // update the time
+    thisObj.timeStart = start;
+    thisObj.timeEnd = end;
   }
-  filterLocations(locations) {
-    // update the locations
+  _toggleLocation(location, update = false) {
+    // location: String(1to19)
+    let thisObj = this;
+    if (thisObj.locations.includes(location)) {
+      // remove the location from the locations, add colour back to the available colours, remove the colour from the used colours
+      let newLocations = thisObj.locations.filter((d) => d !== location);
+      let thisLocationColour = thisObj.locationColour[Number(location)];
+      thisObj.locationColour[Number(location)] =
+        LINE_CHART_DEFAULT_LEGEND_COLOUR;
+      thisObj.usedColours = thisObj.usedColours.filter(
+        (colour) => colour != thisLocationColour
+      );
+      thisObj.availableColours.push(thisLocationColour);
+      return thisObj.filterLocations(newLocations, update);
+    } else {
+      if (thisObj.locations.length < MAX_NUMBER_OF_LOCATION) {
+        // add the location to the locations, remove the colour from the available colours, add the colour to the used colours
+        // assign the colour to the location
+        let newLocations = thisObj.locations;
+        let thisLocationColour = thisObj.availableColours.pop();
+        thisObj.locationColour[Number(location)] = thisLocationColour;
+        thisObj.usedColours.push(thisLocationColour);
+        newLocations.push(location);
+        return thisObj.filterLocations(newLocations, update);
+      } else {
+        // alert the user that the max number of location is 5
+        alert(
+          `The max number of location is ${MAX_NUMBER_OF_LOCATION}, please remove one location first`
+        );
+      }
+    }
+  }
+  _getDamageDataOfLocationByTime(location) {
+    // get the damage data of the location, group by time, using mean value
+    let thisObj = this;
+    let locationData = thisObj.filteredData.filter(
+      (d) => d.location === location
+    );
+    console.log(locationData);
+    // aggregate the damage data group by each time point, using mean value
+    let damageDataOfLocationByTime = {};
+    locationData.forEach((d) => {
+      if (damageDataOfLocationByTime.hasOwnProperty(parseTimeReverse(d.time))) {
+        try {
+          damageDataOfLocationByTime[parseTimeReverse(d.time)].totalDamage +=
+            d.damage_value;
+          damageDataOfLocationByTime[parseTimeReverse(d.time)].count += 1;
+        } catch (error) {
+          console.log("error", error);
+        }
+      } else {
+        damageDataOfLocationByTime[parseTimeReverse(d.time)] = {
+          totalDamage: d.damage_value,
+          count: 1,
+        };
+      }
+    });
+    console.log("damageDataOfLocationByTime", damageDataOfLocationByTime);
+    let damageDataOfLocationByTimeArray = [];
+    for (let time in damageDataOfLocationByTime) {
+      damageDataOfLocationByTimeArray.push({
+        time: parseTime(time),
+        // mean value
+        damage_value:
+          damageDataOfLocationByTime[time].totalDamage /
+          damageDataOfLocationByTime[time].count,
+      });
+    }
+    // sort the array by time
+    damageDataOfLocationByTimeArray.sort((a, b) => a.time - b.time);
+    return damageDataOfLocationByTimeArray;
+  }
+  filterLocations(locations, update = false) {
+    // update the locations, max number of locations is MAX_NUMBER_OF_LOCATION
     let thisObj = this;
     thisObj.locations = locations;
-    thisObj.locationsData = thisObj.locations.map((location) => {
-      return thisObj.filteredData.filter((d) => d.location === location);
-    });
+    for (let location of locations) {
+      //   let locationData = thisObj.filteredData.filter(
+      //     (d) => d.location === location
+      //   );
+      //   console.log(locationData);
+      //   // aggregate the damage data group by each time point, using mean value
+      //   let damageDataOfLocationByTime = {};
+      //   locationData.forEach((d) => {
+      //     if (
+      //       damageDataOfLocationByTime.hasOwnProperty(parseTimeReverse(d.time))
+      //     ) {
+      //       try {
+      //         damageDataOfLocationByTime[parseTimeReverse(d.time)].totalDamage +=
+      //           d.damage_value;
+      //         damageDataOfLocationByTime[parseTimeReverse(d.time)].count += 1;
+      //       } catch (error) {
+      //         console.log("error", error);
+      //       }
+      //     } else {
+      //       damageDataOfLocationByTime[parseTimeReverse(d.time)] = {
+      //         totalDamage: d.damage_value,
+      //         count: 1,
+      //       };
+      //     }
+      //   });
+      //   console.log("damageDataOfLocationByTime", damageDataOfLocationByTime);
+      //   let damageDataOfLocationByTimeArray = [];
+      //   for (let time in damageDataOfLocationByTime) {
+      //     damageDataOfLocationByTimeArray.push({
+      //       time: parseTime(time),
+      //       // mean value
+      //       damage_value:
+      //         damageDataOfLocationByTime[time].totalDamage /
+      //         damageDataOfLocationByTime[time].count,
+      //     });
+      //   }
+      //  // sort the array by time
+      //   damageDataOfLocationByTimeArray.sort((a, b) => a.time - b.time);
+      //  // update the locationsData
+      //   thisObj.locationsData[String(location)] = damageDataOfLocationByTimeArray;
+      thisObj.locationsData[String(location)] =
+        thisObj._getDamageDataOfLocationByTime(location);
+    }
+    if (update) {
+      thisObj.updateVis();
+      thisObj.renderVis();
+    }
   }
 }
--- js/main.js
+++ js/main.js
@@ -2,7 +2,30 @@ let lineChart;
 let stactAreaChart;
 let data;
 const parseTime = d3.timeParse("%Y/%m/%d %H:%M");
+const parseTimeReverse = function (time) {
+  return (
+    time.getUTCFullYear() +
+    "/" +
+    (time.getUTCMonth() + 1) +
+    "/" +
+    time.getUTCDate() +
+    " " +
+    (time.getUTCHours() + 1) +
+    ":" +
+    time.getUTCMinutes()
+  );
+};
 const NUMBER_OF_LOCATION = 19;
+const MAX_NUMBER_OF_LOCATION = 5;
+const HIGHLIGHT_COLOUR = "purple";
+const LINE_CHART_LOCATION_COLOURS = [
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "orange",
+];
+const LINE_CHART_DEFAULT_LEGEND_COLOUR = "white";
 async function main() {
   data = await d3.csv("../data/data_long.csv");
   meanData = await d3.csv("../data/data_mean.csv");
@@ -15,14 +38,22 @@ async function main() {
     d.damage_value = +d.damage_value;
     d.time = parseTime(d.time);
   });
+  meanData.sort((a, b) => a.time - b.time);
 
   lineChart = new LineChart(
     (parentElement = "#lineChartSvg"),
     (tooltipElement = "#lineChartToolTip"),
     (data = data),
     (meanData = meanData),
-    (legendElement = "#lineChartLegend"),
-    (colours = ["#ff0000", "#0000ff"])
+    (legendElement = "#lineChartLegendList"),
+    (colours = LINE_CHART_LOCATION_COLOURS),
+    (changeStackedAreaChart = function (location, timeStart, timeEnd) {
+      document.getElementById(
+        "stackedAreaChartTitle"
+      ).innerHTML = `Stacked Area Chart for ${location} from ${parseTimeReverse(
+        timeStart
+      )} to ${parseTimeReverse(timeEnd)}`;
+    })
   );
   lineChart.initVis();
   lineChart.updateVis();
